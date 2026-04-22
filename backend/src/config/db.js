0ba2efdbed70920e1969f5c4e3dacb2f0066074d
Tobías Coria <tobias_coria@hotmail.com>
2026-04-22 05:18:55 -0300

    fix ssl aiven

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,17 +3,15 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: parseInt(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  ssl: process.env.CA_CERT ? {
-    ca: Buffer.from(process.env.CA_CERT, 'base64')
-  } : process.env.DB_SSL === 'true' ? {
+  ssl: {
     rejectUnauthorized: false
-  } : false,
+  }
 });
 
 module.exports = pool;
